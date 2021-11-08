2e6494eb60bd0a17b4e7697cdc7cd3355dced311
yoonminsohappy@gmail.com <yoonminsohappy@gmail.com>
2021-11-08 18:31:13 +0900

    Updates

---
+++ component---src-pages-404-js-18966798326ae6e5a169.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("I/Ru"),o=n("vrFN");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-18966798326ae6e5a169.js.map
